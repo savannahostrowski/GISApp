df633acb895c2a61fae7f138b62ea3ae6075ab5a
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-03 23:33:41 -0400

    start of html and css along with design

--- js/map.js
+++ js/map.js
@@ -1,11 +1,1 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=
-{pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w}',
- {attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>
-  contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
-   Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'sostrows',
-    accessToken: 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8wn'
-}).addTo(map);
