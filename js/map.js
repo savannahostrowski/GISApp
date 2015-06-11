2422c433bd9d98911d6bf4e95c2a3ba3f8c54a9b
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-10 22:40:02 -0400

    typing feature added

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([43.4643, -80.5], 12);
+// L.mapbox.accessToken = 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w';
+// var map = L.mapbox.map('map', 'mapbox.streets')
+//     .setView([43.4643, -80.5], 12);
+var map = L.map('map').setView([51.505, -0.09], 13);
