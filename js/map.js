f7d3d4a8590fd724699ce51667bfef1555d6bb82
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-13 12:37:49 -0400

    split map progress

--- js/map.js
+++ js/map.js
@@ -1,3 +1,20 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([43.4643, -80.5], 12);
+var map = L.mapbox.map('map');
+L.mapbox.tileLayer('mapbox.outdoors').addTo(map);
+
+var overlay = L.mapbox.tileLayer('mapbox.comic').addTo(map);
+var range = document.getElementById('range');
+
+function clip() {
+  var nw = map.containerPointToLayerPoint([0, 0]),
+      se = map.containerPointToLayerPoint(map.getSize()),
+      clipX = nw.x + (se.x - nw.x) * range.value;
+
+  overlay.getContainer().style.clip = 'rect(' + [nw.y, clipX, se.y, nw.x].join('px,') + 'px)';
+}
+
+range['oninput' in range ? 'oninput' : 'onchange'] = clip;
+map.on('move', clip);
+map.setView([43.4643, -80.5], 12);
+
+clip();
