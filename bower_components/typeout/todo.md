ec2e069de0b43e745cf7644a5348445f85d14b8a
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-15 20:51:09 -0400

    delete

---
+++ bower_components/typeout/todo.md
@@ -0,0 +1,4 @@
+#TODOS
+
+##src/typeout.js
+- Take gif of it working and add it to readme
--- js/typing.js
+++
@@ -1,5 +0,0 @@
-typeout('.typeout', ['sustainable development?','the environment?', 'the economy?', 'society?'], {
-  callback: function(el) {
-    el.innerHTML += ".";
-  }
-});
