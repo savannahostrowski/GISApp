3a72436fb7a536fcad4be0e6c97dee75cba03d54
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-15 21:22:03 -0400

    slider done/progress on multiple layers

--- js/map.js
+++ js/map.js
@@ -1,9 +1,19 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w';
 var map = L.mapbox.map('map');
+//add 2011 data
 L.mapbox.tileLayer('mapbox.outdoors').addTo(map);
 
+//add 1996 data
 var overlay = L.mapbox.tileLayer('mapbox.comic').addTo(map);
+
 var range = document.getElementById('range');
+//to add environment,economic and societal layers
+// var layers = {
+//   Satellite: L.mapbox.tileLayer('mapbox.satellite'),
+//   Streets: L.mapbox.tileLayer('mapbox.streets'),
+//   Outdoors:L.mapbox.tileLayer('mapbox.outdoors')
+// };
+
 
 function clip() {
   var nw = map.containerPointToLayerPoint([0, 0]),
@@ -16,5 +26,7 @@ function clip() {
 range['oninput' in range ? 'oninput' : 'onchange'] = clip;
 map.on('move', clip);
 map.setView([43.4643, -80.5], 12);
+// layers.Streets.addTo(map);
+// L.control.layers(layers).addTo(map);
 
 clip();
