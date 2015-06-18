85361ed9b2f05dd52ddebfa413ceb35838f6c0d4
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-06-17 20:46:47 -0400

    work on ui

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w';
 var map = L.mapbox.map('map');
+
 //add 2011 data
 L.mapbox.tileLayer('mapbox.outdoors').addTo(map);
 
@@ -7,13 +8,37 @@ L.mapbox.tileLayer('mapbox.outdoors').addTo(map);
 var overlay = L.mapbox.tileLayer('mapbox.comic').addTo(map);
 
 var range = document.getElementById('range');
-//to add environment,economic and societal layers
-// var layers = {
-//   Satellite: L.mapbox.tileLayer('mapbox.satellite'),
-//   Streets: L.mapbox.tileLayer('mapbox.streets'),
-//   Outdoors:L.mapbox.tileLayer('mapbox.outdoors')
-// };
 
+//to add environment,economic and societal layers
+var layers = document.getElementById('menu-ui');
+addLayer(L.mapbox.tilelayer('mapbox.streets'), 'Base Map', 1);
+addLayer(L.mapbox.tilelayer('mapbox.comic'), 'Environment', 2);
+addLayer(L.mapbox.tilelayer('mapbox.satellite'), 'Economy', 3);
+addLayer(L.mapbox.tilelayer('mapbox.outdoors'), 'Society', 4);
+addLayer(L.mapbox.tilelayer('mapbox.streets'), 'Total', 5);
+
+
+function addLayer (layer, name, zIndex) {
+  layer
+      .setZIndex(zIndex)
+      .addTo(map);
+
+  var link = document.createElement('a');
+      link.href = '#';
+      link.className = 'active';
+      link.innerHTML = name;
+
+  link.onclick = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if(map.hasLayer(layer)) {
+      map.removeLayer(layer);
+      this.className = '';
+    }
+  };
+  layer.appendChild(link);
+}
 
 function clip() {
   var nw = map.containerPointToLayerPoint([0, 0]),
@@ -26,7 +51,6 @@ function clip() {
 range['oninput' in range ? 'oninput' : 'onchange'] = clip;
 map.on('move', clip);
 map.setView([43.4643, -80.5], 12);
-// layers.Streets.addTo(map);
-// L.control.layers(layers).addTo(map);
+
 
 clip();
