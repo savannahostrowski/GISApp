d2ac88029b10a0ff394b13a6ed9b3aa4c0ed0431
Savannah Ostrowski <savannahostrowski@gmail.com>
2015-07-04 13:42:52 -0400

    omg

--- js/map.js
+++ js/map.js
@@ -8,64 +8,67 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoic29zdHJvd3MiLCJhIjoiYzQzZmM5N2E4MmZiMDFjMWU1ZmE3N2M0M2E2NTllOWUifQ.14jVMAgcp0EglUIjzdyA8w'
 }).addTo(map);
 
+function featureColour (feature) {
+  switch (feature.properties.FINAL_RANK) {
+    case 1: return {
+      color: '#00FF00',
+      fill: '#00FF00',
+      fillOpacity: 1.0,
+    };
+    case 2: return {
+      color: '#55FF00',
+      fill: '#55FF00',
+      fillOpacity: 1.0
+    };
+    case 3: return {
+      color: '#88FF00',
+      fill: '#88FF00',
+      fillOpacity: 1.0
+    };
+    case 4: return {
+      color: '#BBFF00',
+      fill: '#BBFF00',
+      fillOpacity: 1.0
+    };
+    case 5: return {
+      color: '#FFFF00',
+      fill: '#FFFF00',
+      fillOpacity: 1.0
+    };
+    case 6: return {
+      color: '#FFDD00',
+      fill: '#FFDD00',
+      fillOpacity: 1.0
+    };
+    case 7: return {
+      color: '#FFCC00',
+      fill: '#FFCC00',
+      fillOpacity: 1.0
+    };
+    case 8: return {
+      color: '#FF8800',
+      fill: '#FF8800',
+      fillOpacity: 1.0
+    };
+    case 9: return {
+      color: '#FF4400',
+      fill: '#FF4400',
+      fillOpacity: 1.0
+    };
+    case 10: return {
+      color: '#FF0000',
+      fill: '#FF0000',
+      fillOpacity: 1.0
+    };
+  }
+}
 
-function setup (geoJ) {
-  // map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+
+
+function setup (geoJ, paneName) {
   L.geoJson(geoJ, {
-    style: function (feature) {
-      switch (feature.properties.FINAL_RANK) {
-        case 1: return {
-          color: '#00FF00',
-          fill: '#00FF00',
-          fillOpacity: 1.0,
-        };
-        case 2: return {
-          color: '#55FF00',
-          fill: '#55FF00',
-          fillOpacity: 1.0
-        };
-        case 3: return {
-          color: '#88FF00',
-          fill: '#88FF00',
-          fillOpacity: 1.0
-        };
-        case 4: return {
-          color: '#BBFF00',
-          fill: '#BBFF00',
-          fillOpacity: 1.0
-        };
-        case 5: return {
-          color: '#FFFF00',
-          fill: '#FFFF00',
-          fillOpacity: 1.0
-        };
-        case 6: return {
-          color: '#FFDD00',
-          fill: '#FFDD00',
-          fillOpacity: 1.0
-        };
-        case 7: return {
-          color: '#FFCC00',
-          fill: '#FFCC00',
-          fillOpacity: 1.0
-        };
-        case 8: return {
-          color: '#FF8800',
-          fill: '#FF8800',
-          fillOpacity: 1.0
-        };
-        case 9: return {
-          color: '#FF4400',
-          fill: '#FF4400',
-          fillOpacity: 1.0
-        };
-        case 10: return {
-          color: '#FF0000',
-          fill: '#FF0000',
-          fillOpacity: 1.0
-        };
-      }
-    }
+    pane: paneName,
+    style: featureColour
   }).addTo(map);
 
   var overlayPane = map.getPanes().overlayPane;
@@ -90,12 +93,62 @@ function setup (geoJ) {
 
   clip();
 }
-
-$.getJSON('1996final.geojson', function (data) {
+var pane1996 = map.createPane('pane1996');
+$.getJSON('2011final.geojson', function (data) {
   var geoJson1996 = [data];
   // Add features to the map
-  setup(geoJson1996);
+  setup(geoJson1996, 'pane1996');
 });
 
-  var pane2011 = map.createPane('pane2011', map.getPanes().mapPane);
 
+
+
+// function getColor(d) {
+//     return d === 1 ? '#00FF00' :
+//            d === 2 ? '#55FF00' :
+//            d === 3 ? '#88FF00' :
+//            d === 4 ? '#BBFF00' :
+//            d === 5 ? '#FFFF00' :
+//            d === 6 ? '#FFDD00' :
+//            d === 7 ? '#FFCC00' :
+//            d === 8 ? '#FF8800' :
+
+//            d === 9 ? '#FF0000' :
+//            d === 10 ?'#FFEDA0' :
+//            ''
+// }
+
+//      <span style='background:#00FF00;'></span>
+//           <span style='background:#55FF00;'></span>
+//           <span style='background:#88FF00;'></span>
+//           <span style='background:#BBFF00;'></span>
+//           <span style='background:#FFFF00;'></span>
+//           <label>1</label>
+//           <label>2</label>
+//           <label>3</label>
+//           <label>4</label>
+//           <label>5</label>
+//           <span style='background:#FFDD00;'></span>
+//           <span style='background:#FFCC00;'></span>
+//           <span style='background:#FF8800;'></span>
+//           <span style='background:#FF4400;'></span>
+//           <span style='background:#FF0000;'></span>
+
+// var legend = L.control({position: 'bottomright'});
+
+// legend.onAdd = function (map) {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [1,2,3,4,5,6,7,8,9,10],
+//         labels = [];
+
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+//             grades[i] + (grades[i + 1] ? '<br>' : ' ');
+//     }
+
+//     return div;
+// };
+
+// legend.addTo(map);
